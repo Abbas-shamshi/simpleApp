1b5056312c0e3994300e704281b121139249ab85
Abbas Shamshi <abbasshamshi72@gmail.com>
2021-02-02 18:06:09 -0500

    Added Functionality

--- components/speed.js
+++ components/speed.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import {
-    Image
+    ScrollView,
+    Image,
+    View,
+    Text,
+    TextInput,
+    TouchableOpacity
 } from 'react-native';
 import { Picker } from '@react-native-community/picker';
 
@@ -20,110 +25,248 @@ export default class SpeedVC extends Component {
 
     };
 
+    // ================To Navigate=======================
 
-render() {
-    return (
-        <ScrollView>
-            <View style={globalStyle.container}>
-                <View style={globalStyle.headContainer}>
-                    <View style={globalStyle.headTextContainer}>
-                        <Picker
-                            style={globalStyle.pickerHeader}
-                            onValueChange={(value) => this.navigator(value)}
-                        >
-                            <Picker.Item label="Speed Converter" value="Speed" />
-                            <Picker.Item label="Currency Converter" value="Currency" />
-                            <Picker.Item label="Distance Converter" value="Distance" />
-                            <Picker.Item label="Weight Converter" value="Weight" />
-                            <Picker.Item label="Temperature Converter" value="Temperature" />
-                            <Picker.Item label="Volume Converter" value="Volume" />
-                            <Picker.Item label="Age Checker" value="Age" />
+    navigator(value) {
+        console.log("Hello navigation")
 
-                        </Picker>
-                    </View>
-                </View>
+    }
+
+    //   ==================================================
 
 
-                <View style={globalStyle.conversionContainer}>
-                    <View style={globalStyle.imageConatiner}>
-                        <Image
-                            style={globalStyle.currencyImage}
-                            source={{ uri: this.state.exchangeImage }} />
+    // =====================To reset Values ===============
+    clear = () => {
+        this.setState({
+            ValueA: '',
+            ValueB: '',
+        })
+    }
+    // =====================================================
 
-                    </View>
+    // ============Select different conversion options=======
 
-                    <View style={globalStyle.dropDownContainer}>
-                        <Picker
-                            selectedValue={this.state.from}
-                            style={globalStyle.picker}
-                            onValueChange={(itemValue) => this.valuePicker(itemValue, 'picker1')}
-                        >
-                            <Picker.Item label="Mph" value="Mph" />
-                            <Picker.Item label="Kph" value="Kph" />
-                            <Picker.Item label="Knots" value="Knots" />
-                            <Picker.Item label="MPS" value="MPS" />
-                        </Picker>
-                        <Picker
-                            selectedValue={this.state.to}
-                            style={globalStyle.picker}
-                            onValueChange={(itemValue) => this.valuePicker(itemValue, 'picker2')}
-                        >
-                            <Picker.Item label="Mph" value="Mph" />
-                            <Picker.Item label="Kph" value="Kph" />
-                            <Picker.Item label="Knots" value="Knots" />
-                            <Picker.Item label="MPS" value="MPS" />
+    valuePicker(itemValue, pickerNO) {
+        if (pickerNO == 'picker1') {
+            this.setState({
+                from: itemValue,
+                ValueA: '',
+                ValueB: '',
+            })
+        } else if (pickerNO == 'picker2') {
+            this.setState({
+                to: itemValue,
+                ValueA: '',
+                ValueB: '',
+            })
+        }
+    }
+    //   =============================================================
 
-                        </Picker>
-                    </View>
 
-                    <View style={globalStyle.inputBoxContainer}>
-                        <View style={globalStyle.currencyValueContainer}>
-                            <Text style={globalStyle.CurrencyValue}>
-                                {this.state.from}
-                            </Text>
+
+    //==============Identify Which conversion to Calcualate============
+
+    converter(value, input) {
+        console.log(input);
+
+        if (input == 'value1') {
+            this.setState({
+                ValueA: value,
+            })
+            if (this.state.from == 'Mph' && this.state.to == 'Kph') {
+                this.setState({
+                    ValueB: String((parseInt(value) * 1.609).toFixed(2)),
+                });
+                console.log('Mph TO Kph')
+            } else if (this.state.from == 'Mph' && this.state.to == 'Knots') {
+                this.setState({
+                    ValueB: String((parseInt(value) / 1.15).toFixed(2)),
+                });
+                console.log('Mph TO Knots')
+            } else if (this.state.from == 'Mph' && this.state.to == 'MPS') {
+                this.setState({
+                    ValueB: String((parseInt(value) / 2.237).toFixed(2)),
+                });
+                console.log('Mph TO MPS')
+            } else if (this.state.from == 'Kph' && this.state.to == 'Mph') {
+                this.setState({
+                    ValueB: String((parseInt(value) / 1.609).toFixed(2)),
+                });
+                console.log('Kph TO Mph')
+            } else if (this.state.from == 'Kph' && this.state.to == 'Knots') {
+                this.setState({
+                    ValueB: String((parseInt(value) / 1.852).toFixed(2)),
+                });
+                console.log('Kph TO Knots')
+            } else if (this.state.from == 'Kph' && this.state.to == 'MPS') {
+                this.setState({
+                    ValueB: String((parseInt(value) / 3.6).toFixed(2)),
+                });
+                console.log('Kph TO MPS')
+            } else if (this.state.from == 'Knots' && this.state.to == 'Mph') {
+                this.setState({
+                    ValueB: String((parseInt(value) * 1.15).toFixed(2)),
+                });
+                console.log('Knots TO Mph')
+            } else if (this.state.from == 'Knots' && this.state.to == 'Kph') {
+                this.setState({
+                    ValueB: String((parseInt(value) * 1.852).toFixed(2)),
+                });
+                console.log('Knots TO Kph')
+            } else if (this.state.from == 'Knots' && this.state.to == 'MPS') {
+                this.setState({
+                    ValueB: String((parseInt(value) / 1.944).toFixed(2)),
+                });
+                console.log('Knots TO MPS')
+            } else if (this.state.from == 'MPS' && this.state.to == 'Mph') {
+                this.setState({
+                    ValueB: String((parseInt(value) / 1000).toFixed(2)),
+                });
+                console.log('MPS TO Mph')
+            } else if (this.state.from == 'MPS' && this.state.to == 'Kph') {
+                this.setState({
+                    ValueB: String((parseInt(value) * 100).toFixed(2)),
+                });
+                console.log('MPS TO Kph')
+            } else if (this.state.from == 'MPS' && this.state.to == 'Knots') {
+                this.setState({
+                    ValueB: String((parseInt(value) / 1609).toFixed(2)),
+                });
+                console.log('MPS TO Knots')
+            } else if (this.state.from == 'MPS' && this.state.to == 'MPS') {
+                this.setState({
+                    ValueB: value,
+                });
+                console.log('MPS TO MPS')
+            } else if (this.state.from == 'Knots' && this.state.to == 'Knots') {
+                this.setState({
+                    ValueB: value,
+                });
+                console.log('Knots TO Knots')
+            } else if (this.state.from == 'Mph' && this.state.to == 'Mph') {
+                this.setState({
+                    ValueB: value,
+                });
+                console.log('Mph TO Mph')
+            } else if (this.state.from == 'Kph' && this.state.to == 'Kph') {
+                this.setState({
+                    ValueB: value,
+                });
+                console.log('Kph TO Kph')
+            }
+
+        }
+
+    }
+
+    //   =================================================================
+
+
+
+    render() {
+        return (
+            <ScrollView>
+                <View style={globalStyle.container}>
+                    <View style={globalStyle.headContainer}>
+                        <View style={globalStyle.headTextContainer}>
+                            <Picker
+                                style={globalStyle.pickerHeader}
+                                onValueChange={(value) => this.navigator(value)}
+                            >
+                                <Picker.Item label="Speed Converter" value="Speed" />
+                                <Picker.Item label="Currency Converter" value="Currency" />
+                                <Picker.Item label="Distance Converter" value="Distance" />
+                                <Picker.Item label="Weight Converter" value="Weight" />
+                                <Picker.Item label="Temperature Converter" value="Temperature" />
+                                <Picker.Item label="Volume Converter" value="Volume" />
+                                <Picker.Item label="Age Checker" value="Age" />
+
+                            </Picker>
                         </View>
-                        <TextInput
-                            style={globalStyle.inputBox}
-                            placeholder={'Enter Value'}
-
-                            keyboardType={'numeric'}
-                            value={this.state.ValueA}
-                            onChangeText={(value) => this.converter(value, 'value1')}
-                        />
-
-
                     </View>
-                    <View style={globalStyle.inputBoxContainer}>
-                        <View style={globalStyle.currencyValueContainer}>
-                            <Text style={globalStyle.CurrencyValue}>
-                                {this.state.to}
-                            </Text>
+
+
+                    <View style={globalStyle.conversionContainer}>
+                        <View style={globalStyle.imageConatiner}>
+                            <Image
+                                style={globalStyle.currencyImage}
+                                source={{ uri: this.state.exchangeImage }} />
+
                         </View>
-                        <TextInput
-                            editable={false}
-                            style={globalStyle.inputBox}
-                            keyboardType={'numeric'}
-                            value={this.state.ValueB}
-                            onChangeText={(value) => this.converter(value, 'value2')}
-                        />
 
+                        <View style={globalStyle.dropDownContainer}>
+                            <Picker
+                                selectedValue={this.state.from}
+                                style={globalStyle.picker}
+                                onValueChange={(itemValue) => this.valuePicker(itemValue, 'picker1')}
+                            >
+                                <Picker.Item label="Mph" value="Mph" />
+                                <Picker.Item label="Kph" value="Kph" />
+                                <Picker.Item label="Knots" value="Knots" />
+                                <Picker.Item label="MPS" value="MPS" />
+                            </Picker>
+                            <Picker
+                                selectedValue={this.state.to}
+                                style={globalStyle.picker}
+                                onValueChange={(itemValue) => this.valuePicker(itemValue, 'picker2')}
+                            >
+                                <Picker.Item label="Mph" value="Mph" />
+                                <Picker.Item label="Kph" value="Kph" />
+                                <Picker.Item label="Knots" value="Knots" />
+                                <Picker.Item label="MPS" value="MPS" />
 
-                    </View>
-                    <TouchableOpacity
-                        onPress={this.clear}>
+                            </Picker>
+                        </View>
+
                         <View style={globalStyle.inputBoxContainer}>
-                            <View style={globalStyle.button} >
-                                <Text style={globalStyle.btnText}>
-                                    Clear
-                  </Text>
+                            <View style={globalStyle.currencyValueContainer}>
+                                <Text style={globalStyle.CurrencyValue}>
+                                    {this.state.from}
+                                </Text>
                             </View>
+                            <TextInput
+                                style={globalStyle.inputBox}
+                                placeholder={'Enter Value'}
+
+                                keyboardType={'numeric'}
+                                value={this.state.ValueA}
+                                onChangeText={(value) => this.converter(value, 'value1')}
+                            />
+
 
                         </View>
-                    </TouchableOpacity>
+                        <View style={globalStyle.inputBoxContainer}>
+                            <View style={globalStyle.currencyValueContainer}>
+                                <Text style={globalStyle.CurrencyValue}>
+                                    {this.state.to}
+                                </Text>
+                            </View>
+                            <TextInput
+                                editable={false}
+                                style={globalStyle.inputBox}
+                                keyboardType={'numeric'}
+                                value={this.state.ValueB}
+                                onChangeText={(value) => this.converter(value, 'value2')}
+                            />
 
+
+                        </View>
+                        <TouchableOpacity
+                            onPress={this.clear}>
+                            <View style={globalStyle.inputBoxContainer}>
+                                <View style={globalStyle.button} >
+                                    <Text style={globalStyle.btnText}>
+                                        Clear
+                  </Text>
+                                </View>
+
+                            </View>
+                        </TouchableOpacity>
+
+                    </View>
                 </View>
-            </View>
-        </ScrollView>
-    );
-}
+            </ScrollView>
+        );
+    }
 }
