b5dbc866d454e4d4ff08b7641371b04aa7a88645
Abbas Shamshi <abbasshamshi72@gmail.com>
2021-02-02 18:49:39 -0500

    Commented for better Readability

--- components/temperature.js
+++ components/temperature.js
@@ -1,132 +1,155 @@
 import React, { Component } from 'react';
 import {
-  Text,
-  View,
-  TouchableOpacity,
-  Image,
-  ScrollView,
-  TextInput,
+    Text,
+    View,
+    TouchableOpacity,
+    Image,
+    ScrollView,
+    TextInput,
 } from 'react-native';
-import {Picker} from '@react-native-community/picker';
+import { Picker } from '@react-native-community/picker';
 
 import globalStyle from '../style'
 
 export default class TemperatureVC extends Component {
-  constructor(props) {
-    super(props);
-  }
-  state = {
-    ValueA: '',
-    ValueB: '',
-    from: 'Celsius',
-    to: 'Kelvin',
-    exchangeImage: 'https://newtonfoxbds.com/wp-content/uploads/2017/01/Two_way-data-exchange.gif',
-  };
+    constructor(props) {
+        super(props);
+    }
+    state = {
+        ValueA: '',
+        ValueB: '',
+        from: 'Celsius',
+        to: 'Kelvin',
+        exchangeImage: 'https://newtonfoxbds.com/wp-content/uploads/2017/01/Two_way-data-exchange.gif',
+    };
+
+    // ================To Navigate=======================
+
+    navigator(value) {
+        console.log("Hello navigation")
+
+    }
+
+    //   ==================================================
+
+
+    // =====================To reset Values ===============
+    clear = () => {
+        this.setState({
+            ValueA: '',
+            ValueB: '',
+        })
+    }
+    // =====================================================
 
 
 
-  render() {
-    return (
-      <ScrollView>
-        <View style={globalStyle.container}>
-          <View style={globalStyle.headContainer}>
-            <View style={globalStyle.headTextContainer}>
-              <Picker
-                style={globalStyle.pickerHeader}
-                onValueChange={(value) => this.navigator(value)}
-              >
-                <Picker.Item label="Temperature Converter" value="Temperature" />
-                <Picker.Item label="Currency Converter" value="Currency" />
-                <Picker.Item label="Distance Converter" value="Distance" />
-                <Picker.Item label="Weight Converter" value="Weight" />
-                <Picker.Item label="Speed Converter" value="Speed" />
-                <Picker.Item label="Volume Converter" value="Volume" />
-                <Picker.Item label="Age Checker" value="Age" />
+    render() {
+        return (
+            <ScrollView>
+                <View style={globalStyle.container}>
+                    <View style={globalStyle.headContainer}>
+                        <View style={globalStyle.headTextContainer}>
 
-              </Picker>
-            </View>
-          </View>
+                            {/* Navigate Different Pages */}
+                            <Picker
+                                style={globalStyle.pickerHeader}
+                                onValueChange={(value) => this.navigator(value)}
+                            >
+                                <Picker.Item label="Temperature Converter" value="Temperature" />
+                                <Picker.Item label="Currency Converter" value="Currency" />
+                                <Picker.Item label="Distance Converter" value="Distance" />
+                                <Picker.Item label="Weight Converter" value="Weight" />
+                                <Picker.Item label="Speed Converter" value="Speed" />
+                                <Picker.Item label="Volume Converter" value="Volume" />
+                                <Picker.Item label="Age Checker" value="Age" />
 
-
-          <View style={globalStyle.conversionContainer}>
-            <View style={globalStyle.imageConatiner}>
-              <Image
-                style={globalStyle.currencyImage}
-                source={{ uri: this.state.exchangeImage }} />
-
-            </View>
-
-            <View style={globalStyle.dropDownContainer}>
-              <Picker
-                selectedValue={this.state.from}
-                style={globalStyle.picker}
-                onValueChange={(itemValue) => this.valuePicker(itemValue, 'picker1')}
-              >
-                <Picker.Item label="Celsius" value="Celsius" />
-                <Picker.Item label="Kelvin" value="Kelvin" />
-                <Picker.Item label="Fahrenheit " value="Frhn" />
-              </Picker>
-              <Picker
-                selectedValue={this.state.to}
-                style={globalStyle.picker}
-                onValueChange={(itemValue) => this.valuePicker(itemValue, 'picker2')}
-              >
-                <Picker.Item label="Celsius" value="Celsius" />
-                <Picker.Item label="Kelvin" value="Kelvin" />
-                <Picker.Item label="Fahrenheit " value="Frhn" />
-
-              </Picker>
-            </View>
-
-            <View style={globalStyle.inputBoxContainer}>
-              <View style={globalStyle.currencyValueContainer}>
-                <Text style={globalStyle.CurrencyValue}>
-                  {this.state.from}
-                </Text>
-              </View>
-              <TextInput
-                style={globalStyle.inputBox}
-                keyboardType={'numeric'}
-                placeholder={'Enter Value'}
-
-                value={this.state.ValueA}
-                onChangeText={(value) => this.converter(value, 'value1')}
-              />
+                            </Picker>
+                        </View>
+                    </View>
 
 
-            </View>
-            <View style={globalStyle.inputBoxContainer}>
-              <View style={globalStyle.currencyValueContainer}>
-                <Text style={globalStyle.CurrencyValue}>
-                  {this.state.to}
-                </Text>
-              </View>
-              <TextInput
-                editable={false}
+                    <View style={globalStyle.conversionContainer}>
+                        <View style={globalStyle.imageConatiner}>
+                            <Image
+                                style={globalStyle.currencyImage}
+                                source={{ uri: this.state.exchangeImage }} />
+                        </View>
 
-                style={globalStyle.inputBox}
-                keyboardType={'numeric'}
-                value={this.state.ValueB}
-                onChangeText={(value) => this.converter(value, 'value2')}
-              />
+                        {/* Select Different Conversion Options */}
+                        <View style={globalStyle.dropDownContainer}>
+                            <Picker
+                                selectedValue={this.state.from}
+                                style={globalStyle.picker}
+                                onValueChange={(itemValue) => this.valuePicker(itemValue, 'picker1')}
+                            >
+                                <Picker.Item label="Celsius" value="Celsius" />
+                                <Picker.Item label="Kelvin" value="Kelvin" />
+                                <Picker.Item label="Fahrenheit " value="Frhn" />
+                            </Picker>
+                            <Picker
+                                selectedValue={this.state.to}
+                                style={globalStyle.picker}
+                                onValueChange={(itemValue) => this.valuePicker(itemValue, 'picker2')}
+                            >
+                                <Picker.Item label="Celsius" value="Celsius" />
+                                <Picker.Item label="Kelvin" value="Kelvin" />
+                                <Picker.Item label="Fahrenheit " value="Frhn" />
+
+                            </Picker>
+                        </View>
+
+                        {/* Accept input for conversion */}
+                        <View style={globalStyle.inputBoxContainer}>
+                            <View style={globalStyle.currencyValueContainer}>
+                                <Text style={globalStyle.CurrencyValue}>
+                                    {this.state.from}
+                                </Text>
+                            </View>
+                            <TextInput
+                                style={globalStyle.inputBox}
+                                keyboardType={'numeric'}
+                                placeholder={'Enter Value'}
+
+                                value={this.state.ValueA}
+                                onChangeText={(value) => this.converter(value, 'value1')}
+                            />
 
 
-            </View>
-            <TouchableOpacity
-              onPress={this.clear}>
-              <View style={globalStyle.inputBoxContainer}>
-                <View style={globalStyle.button} >
-                  <Text style={globalStyle.btnText}>
-                    Clear
+                        </View>
+                        <View style={globalStyle.inputBoxContainer}>
+                            <View style={globalStyle.currencyValueContainer}>
+                                <Text style={globalStyle.CurrencyValue}>
+                                    {this.state.to}
+                                </Text>
+                            </View>
+                            <TextInput
+                                editable={false}
+
+                                style={globalStyle.inputBox}
+                                keyboardType={'numeric'}
+                                value={this.state.ValueB}
+                                onChangeText={(value) => this.converter(value, 'value2')}
+                            />
+
+
+                        </View>
+                        {/* To Clear the Inputs */}
+                        <TouchableOpacity
+                            onPress={this.clear}>
+                            <View style={globalStyle.inputBoxContainer}>
+                                <View style={globalStyle.button} >
+                                    <Text style={globalStyle.btnText}>
+                                        Clear
                   </Text>
+                                </View>
+
+                            </View>
+                        </TouchableOpacity>
+
+                    </View>
                 </View>
-
-              </View>
-            </TouchableOpacity>
-
-          </View>
-        </View>
-      </ScrollView>
-    );
-  }
+            </ScrollView>
+        );
+    }
 }
