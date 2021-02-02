c01cf2203ff50f9302942eb06add1c7ea5b6e3e8
Abbas Shamshi <abbasshamshi72@gmail.com>
2021-02-01 19:53:34 -0500

    Created Distance Component
    Created distance Page which calculates basic distance conversions

--- App.js
+++ App.js
@@ -6,7 +6,7 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, { Fragment } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -23,52 +23,16 @@ import {
   DebugInstructions,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import DistanceVC from './components/distance';
 
-const App: () => React$Node = () => {
+const App = () => {
   return (
-    <>
+    <Fragment>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+
+      <DistanceVC/>
+
+    </Fragment >
   );
 };
 
---
+++ components/distance.js
@@ -0,0 +1,306 @@
+import React, { Component } from 'react';
+import {
+    Text,
+    View,
+    TouchableOpacity,
+    Image,
+    Dimensions,
+    ScrollView,
+    TextInput,
+} from 'react-native';
+import { Picker } from '@react-native-community/picker';
+
+import globalStyle from '../style'
+
+export default class DistanceVC extends Component {
+    constructor(props) {
+        super(props);
+    }
+    state = {
+
+        ValueA: '',
+        ValueB: '',
+        from: 'KM',
+        to: 'CM',
+        exchangeImage: 'https://newtonfoxbds.com/wp-content/uploads/2017/01/Two_way-data-exchange.gif',
+        navName: "Distance"
+    };
+
+    //  ======================Function To Calculate Different Distance Metric Conversion ===================
+    kmToMiles = (valueA) => {
+        this.setState({
+            valueA,
+            valueB: String((parseInt(valueA) / 1.609).toFixed(2)),
+        });
+    };
+    milesToKm = (valueB) => {
+        this.setState({
+            valueB,
+            valueA: String((parseInt(valueB) * 1.609).toFixed(2)),
+        });
+    };
+    cmToMeter = (valueC) => {
+        this.setState({
+            valueC,
+            valueD: String((parseInt(valueC) / 100).toFixed(2)),
+        });
+    };
+    meterToCm = (valueD) => {
+        this.setState({
+            valueD,
+            valueC: String((parseInt(valueD) * 100).toFixed(2)),
+        });
+    };
+
+    //=============================================================================================
+
+
+
+    //====================Identify Which conversion to Calcualate==================
+
+    converter(value, input) {
+        console.log(input);
+
+        if (input == 'value1') {
+            this.setState({
+                ValueA: value,
+            })
+            if (this.state.from == 'KM' && this.state.to == 'CM') {
+                this.setState({
+                    ValueB: String((parseInt(value) * 100000).toFixed(2)),
+                });
+                console.log('KM TO CM')
+            } else if (this.state.from == 'KM' && this.state.to == 'Mile') {
+                this.setState({
+                    ValueB: String((parseInt(value) / 1.609).toFixed(2)),
+                });
+                console.log('KM TO Mile')
+            } else if (this.state.from == 'KM' && this.state.to == 'Meter') {
+                this.setState({
+                    ValueB: String((parseInt(value) * 1000).toFixed(2)),
+                });
+                console.log('KM TO Meter')
+            } else if (this.state.from == 'CM' && this.state.to == 'KM') {
+                this.setState({
+                    ValueB: String((parseInt(value) / 100000).toFixed(2)),
+                });
+                console.log('CM TO KM')
+            } else if (this.state.from == 'CM' && this.state.to == 'Mile') {
+                this.setState({
+                    ValueB: String((parseInt(value) / 160934).toFixed(2)),
+                });
+                console.log('CM TO Mile')
+            } else if (this.state.from == 'CM' && this.state.to == 'Meter') {
+                this.setState({
+                    ValueB: String((parseInt(value) / 100).toFixed(2)),
+                });
+                console.log('CM TO Meter')
+            } else if (this.state.from == 'Mile' && this.state.to == 'KM') {
+                this.setState({
+                    ValueB: String((parseInt(value) * 1.60).toFixed(2)),
+                });
+                console.log('Mile TO KM')
+            } else if (this.state.from == 'Mile' && this.state.to == 'CM') {
+                this.setState({
+                    ValueB: String((parseInt(value) * 160934).toFixed(2)),
+                });
+                console.log('Mile TO CM')
+            } else if (this.state.from == 'Mile' && this.state.to == 'Meter') {
+                this.setState({
+                    ValueB: String((parseInt(value) * 1609).toFixed(2)),
+                });
+                console.log('Mile TO Meter')
+            } else if (this.state.from == 'Meter' && this.state.to == 'KM') {
+                this.setState({
+                    ValueB: String((parseInt(value) / 1000).toFixed(2)),
+                });
+                console.log('Meter TO KM')
+            } else if (this.state.from == 'Meter' && this.state.to == 'CM') {
+                this.setState({
+                    ValueB: String((parseInt(value) * 100).toFixed(2)),
+                });
+                console.log('Meter TO CM')
+            } else if (this.state.from == 'Meter' && this.state.to == 'Mile') {
+                this.setState({
+                    ValueB: String((parseInt(value) / 1609).toFixed(2)),
+                });
+                console.log('Meter TO Mile')
+            } else if (this.state.from == 'Meter' && this.state.to == 'Meter') {
+                this.setState({
+                    ValueB: value,
+                });
+                console.log('Meter TO Meter')
+            } else if (this.state.from == 'Mile' && this.state.to == 'Mile') {
+                this.setState({
+                    ValueB: value,
+                });
+                console.log('Mile TO Mile')
+            } else if (this.state.from == 'KM' && this.state.to == 'KM') {
+                this.setState({
+                    ValueB: value,
+                });
+                console.log('KM TO KM')
+            } else if (this.state.from == 'CM' && this.state.to == 'CM') {
+                this.setState({
+                    ValueB: value,
+                });
+                console.log('CM TO CM')
+            }
+
+        }
+
+        // =========================================================================
+
+
+        // ================Select different conversion options=======================
+
+    }
+    valuePicker(itemValue, pickerNO) {
+        if (pickerNO == 'picker1') {
+            this.setState({
+                from: itemValue,
+                ValueA: '',
+                ValueB: '',
+            })
+        } else if (pickerNO == 'picker2') {
+            this.setState({
+                to: itemValue,
+                ValueA: '',
+                ValueB: '',
+            })
+        }
+    }
+
+    //   =================================================
+
+    // =============To Navigate================
+
+    navigator(value) {
+        console.log("Hello navigation")
+
+    }
+
+    //   ====================================================
+
+
+    // =====================To reset Values ==============
+    clear = () => {
+        this.setState({
+            ValueA: '',
+            ValueB: '',
+        })
+    }
+
+    //   =============================
+    render() {
+        return (
+            <ScrollView>
+                <View style={globalStyle.container}>
+                    <View style={globalStyle.headContainer}>
+                        <View style={globalStyle.headTextContainer}>
+                            {/* Navigate Different Pages */}
+                            <Picker
+                                selectedValue={this.state.navName}
+                                style={globalStyle.pickerHeader}
+                                onValueChange={(value) => this.navigator(value)}
+                            >
+                                <Picker.Item label="Distance Converter" value="Distance" />
+                                <Picker.Item label="Currency Converter" value="Currency" />
+                                <Picker.Item label="Weight Converter" value="Weight" />
+                                <Picker.Item label="Temperature Converter" value="Temperature" />
+                                <Picker.Item label="Speed Converter" value="Speed" />
+                                <Picker.Item label="Volume Converter" value="Volume" />
+                                <Picker.Item label="Age Checker" value="Age" />
+
+                            </Picker>
+                        </View>
+                    </View>
+
+
+                    <View style={globalStyle.conversionContainer}>
+                        <View style={globalStyle.imageConatiner}>
+                            <Image
+                                style={globalStyle.currencyImage}
+                                source={{ uri: this.state.exchangeImage }} />
+
+                        </View>
+                        {/* Select Differewnt Conversion Options */}
+                        <View style={globalStyle.dropDownContainer}>
+                            <Picker
+                                selectedValue={this.state.from}
+                                style={globalStyle.picker}
+                                onValueChange={(itemValue) => this.valuePicker(itemValue, 'picker1')}
+                            >
+                                <Picker.Item label="KM" value="KM" />
+                                <Picker.Item label="Mile" value="Mile" />
+                                <Picker.Item label="CM" value="CM" />
+                                <Picker.Item label="Meter" value="Meter" />
+
+                            </Picker>
+                            <Picker
+                                selectedValue={this.state.to}
+                                style={globalStyle.picker}
+                                onValueChange={(itemValue) => this.valuePicker(itemValue, 'picker2')}
+                            >
+                                <Picker.Item label="KM" value="KM" />
+                                <Picker.Item label="Mile" value="Mile" />
+                                <Picker.Item label="CM" value="CM" />
+                                <Picker.Item label="Meter" value="Meter" />
+
+                            </Picker>
+                        </View>
+
+                        {/* Accept input for conversion */}
+                        <View style={globalStyle.inputBoxContainer}>
+                            <View style={globalStyle.currencyValueContainer}>
+                                <Text style={globalStyle.CurrencyValue}>
+                                    {this.state.from}
+                                </Text>
+                            </View>
+                            <TextInput
+                                style={globalStyle.inputBox}
+                                placeholder={'Enter Value'}
+
+                                keyboardType={'numeric'}
+                                value={this.state.ValueA}
+                                onChangeText={(value) => this.converter(value, 'value1')}
+                            />
+
+
+                        </View>
+                        <View style={globalStyle.inputBoxContainer}>
+                            <View style={globalStyle.currencyValueContainer}>
+                                <Text style={globalStyle.CurrencyValue}>
+                                    {this.state.to}
+                                </Text>
+                            </View>
+                            <TextInput
+                                editable={false}
+                                style={globalStyle.inputBox}
+                                keyboardType={'numeric'}
+                                value={this.state.ValueB}
+                                onChangeText={(value) => this.converter(value, 'value2')}
+                            />
+
+                        </View>
+                        {/* To Clear the Inputs */}
+                        <TouchableOpacity
+                            onPress={this.clear}>
+                            <View style={globalStyle.inputBoxContainer}>
+                                <View style={globalStyle.button} >
+                                    <Text style={globalStyle.btnText}>
+                                        Clear
+                  </Text>
+                                </View>
+
+                            </View>
+                        </TouchableOpacity>
+
+                    </View>
+                </View>
+            </ScrollView>
+        );
+    }
+}
+
+
