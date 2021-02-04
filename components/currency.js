0613b53218ee10e615dff6fd884437b03df007b8
Abbas Shamshi <abbasshamshi72@gmail.com>
2021-02-03 22:29:12 -0500

    Currency Page

---
+++ components/currency.js
@@ -0,0 +1,145 @@
+import React, { Component } from 'react';
+import {
+    Text,
+    View,
+    TouchableOpacity,
+    Image,
+    ScrollView,
+    TextInput,
+} from 'react-native';
+// import axios from 'axios';
+import { Picker } from '@react-native-community/picker';
+
+import globalStyle from '../style'
+
+export default class Currency extends Component {
+    constructor(props) {
+        super(props);
+    }
+    state = {
+
+        ValueA: '',
+        ValueB: '',
+        from: 'USD',
+        to: 'INR',
+        exchangeImage: 'https://newtonfoxbds.com/wp-content/uploads/2017/01/Two_way-data-exchange.gif',
+    };
+
+    render() {
+        return (
+            <ScrollView>
+                <View style={globalStyle.container}>
+                    <View style={globalStyle.headContainer}>
+                        <View style={globalStyle.headTextContainer}>
+
+                            {/* Navigate Different Pages */}
+                            <Picker
+                                selectedValue={this.state.fromCurrency}
+                                style={globalStyle.pickerHeader}
+                                onValueChange={(value) => this.navigator(value)}
+                            >
+                                <Picker.Item label="Currency Converter" value="Currency" />
+                                <Picker.Item label="Distance Converter" value="Distance" />
+                                <Picker.Item label="Weight Converter" value="Weight" />
+                                <Picker.Item label="Temperature Converter" value="Temperature" />
+                                <Picker.Item label="Speed Converter" value="Speed" />
+                                <Picker.Item label="Volume Converter" value="Volume" />
+                                <Picker.Item label="Age Checker" value="Age" />
+                            </Picker>
+                        </View>
+                    </View>
+                    {/* Exchange Image==== */}
+                    <View style={globalStyle.conversionContainer}>
+                        <View style={globalStyle.imageConatiner}>
+                            <Image
+                                style={globalStyle.currencyImage}
+                                source={{ uri: this.state.exchangeImage }} />
+                        </View>
+
+                        {/* Select Different Conversion Options */}
+                        <View style={globalStyle.dropDownContainer}>
+                            <Picker
+                                selectedValue={this.state.from}
+                                style={globalStyle.picker}
+                                onValueChange={(itemValue) => this.setState({ from: itemValue })}
+                            >
+                                <Picker.Item label="USD Dollar" value="USD" />
+                                <Picker.Item label="Indian Rupee" value="INR" />
+                                <Picker.Item label="Euro" value="EUR" />
+                                <Picker.Item label="Austrailan Dollar" value="AUD" />
+                                <Picker.Item label="Canadian Dollar" value="CAD" />
+                                <Picker.Item label="Japanese Yen" value="JPY" />
+                                <Picker.Item label="New Zeland Dollar" value="NZD" />
+                                <Picker.Item label="Russian Ruble" value="RUB" />
+                                <Picker.Item label="Honk Kong Dollar" value="HKD" />
+                            </Picker>
+                            <Picker
+                                selectedValue={this.state.to}
+                                style={globalStyle.picker}
+                                onValueChange={(itemValue) => this.setState({ to: itemValue })}
+                            >
+                                <Picker.Item label="USD Dollar" value="USD" />
+                                <Picker.Item label="Indian Rupee" value="INR" />
+                                <Picker.Item label="Euro" value="EUR" />
+                                <Picker.Item label="Austrailan Dollar" value="AUD" />
+                                <Picker.Item label="Canadian Dollar" value="CAD" />
+                                <Picker.Item label="Japanese Yen" value="JPY" />
+                                <Picker.Item label="New Zeland Dollar" value="NZD" />
+                                <Picker.Item label="Russian Ruble" value="RUB" />
+                                <Picker.Item label="Honk Kong Dollar" value="HKD" />
+                            </Picker>
+                        </View>
+
+                        {/* Accept input for conversion */}
+                        <View style={globalStyle.inputBoxContainer}>
+                            <View style={globalStyle.currencyValueContainer}>
+                                <Text style={globalStyle.CurrencyValue}>
+                                    {this.state.from}
+                                </Text>
+                            </View>
+                            <TextInput
+                                style={globalStyle.inputBox}
+                                placeholder={'Enter Value'}
+
+                                keyboardType={'numeric'}
+                                value={this.state.ValueA}
+                                onChangeText={this.converterA}
+                            />
+                        </View>
+
+                        {/* Accept input for conversion */}
+                        <View style={globalStyle.inputBoxContainer}>
+                            <View style={globalStyle.currencyValueContainer}>
+                                <Text style={globalStyle.CurrencyValue}>
+                                    {this.state.to}
+                                </Text>
+                            </View>
+                            <TextInput
+                                style={globalStyle.inputBox}
+                                placeholder={'Enter Value'}
+
+                                keyboardType={'numeric'}
+                                value={this.state.ValueB}
+                                onChangeText={(value) => this.converterB(value)}
+                            />
+                        </View>
+
+                        {/* To Clear the Inputs */}
+                        <TouchableOpacity
+                            onPress={this.clear}>
+                            <View style={globalStyle.inputBoxContainer}>
+                                <View style={globalStyle.button} >
+                                    <Text style={globalStyle.btnText}>
+                                        Clear
+                                    </Text>
+                                </View>
+                            </View>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </ScrollView>
+        );
+    }
+}
+
+
