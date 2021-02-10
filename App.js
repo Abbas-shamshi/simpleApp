6e4eb5ecbf0bd1805772a3439177931dbe0236b1
Abbas Shamshi <abbasshamshi72@gmail.com>
2021-02-09 19:17:37 -0500

    Using react Navigation to Navigate

--- App.js
+++ App.js
@@ -1,36 +1,48 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
+import 'react-native-gesture-handler';
 import React, { Fragment } from 'react';
 import {
-  SafeAreaView,
   StyleSheet,
-  ScrollView,
-  View,
-  Text,
   StatusBar,
 } from 'react-native';
 
 import {
-  Header,
-  LearnMoreLinks,
   Colors,
-  DebugInstructions,
-  ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
 import DistanceVC from './components/distance';
+import SpeedVC from './components/speed';
+import TemperatureVC from './components/temperature';
+import VolumeVC from './components/volume';
+import WeightVC from './components/weight';
+import Currency from './components/currency';
+import AgeChecker from './components/ageChecker';
+import HomeScreen from './components/homeScreen';
+
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <Fragment>
       <StatusBar barStyle="dark-content" />
 
-      <DistanceVC/>
+
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{headerShown: true}} initialRouteName="Home">
+          <Stack.Screen options={{ headerShown: false }} name="Home" component={HomeScreen} />
+          <Stack.Screen name="Currency" component={Currency} />
+          <Stack.Screen name="Distance" component={DistanceVC} />
+          <Stack.Screen name="Volume" component={VolumeVC} />
+          <Stack.Screen name="Speed" component={SpeedVC} />
+          <Stack.Screen name="Temperature" component={TemperatureVC} />
+          <Stack.Screen name="Weight" component={WeightVC} />
+          <Stack.Screen name="Age" component={AgeChecker} />
+
+        </Stack.Navigator>
+      </NavigationContainer>
 
     </Fragment >
   );
